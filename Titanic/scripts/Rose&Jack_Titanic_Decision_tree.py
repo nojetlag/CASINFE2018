25ad064fd2a4721771f63d0e129694f9940ad97a
Isabelle Kluser <isabelle.kluser@gmail.com>
2018-10-29 09:48:06 +0100

    sibsp und parch hinzugefügt
    features = train[["pclass", "sex", "age", "fare","sibsp","parch"]].values
    
    sibsp und parch hinzugefügt

--- Titanic/scripts/Rose&Jack_Titanic_Decision_tree.py
+++ Titanic/scripts/Rose&Jack_Titanic_Decision_tree.py
@@ -21,7 +21,7 @@ print ("\nExtracting target and features")
 
 print(train.shape)
 target = train["survived"].values
-features = train[["pclass", "sex", "age", "fare"]].values
+features = train[["pclass", "sex", "age", "fare","sibsp","parch"]].values
 
 decision_tree = tree.DecisionTreeClassifier(random_state = 1)
 decision_tree = decision_tree.fit(features, target)
