6adbaf3fc224aba59bf6ef15fdc6045e1bd16ecc
= <=>
2018-11-05 09:06:10 +0100

    cleanup code and remove os dependent paths

--- Titanic/scripts/Rose_and_Jack_Titanic_Decision_tree_post.py
+++ Titanic/scripts/Rose_and_Jack_Titanic_Decision_tree_post.py
@@ -1,68 +1,60 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Oct 29 09:45 2018
-Team Rose & Jack
-3 
-
+Team Rose & Jack 
 """
 
 import l_utils as utils
 import l_plot as plot
 import pandas as pd
 from sklearn import tree, model_selection
+from pathlib import Path
 
-train = pd.read_csv('../data/titanic3_train.csv', sep=';')
-test = pd.read_csv('../data/titanic3_test.csv', sep=';')
+#prepare all used files to be os independent
+project_path = Path(__file__).parents[1]
+testfile = Path.joinpath(project_path, "data/titanic3_test.csv")
+trainfile = Path.joinpath(project_path, "data/titanic3_train.csv")
+plotfile = Path.joinpath(project_path, "presentation/Rose_and_Jack_post.out")
+submissionfile = Path.joinpath(project_path, "submissions/Rose_and_Jack_post.csv")
 
+#load data files for test and train 
+test = pd.read_csv(testfile, sep=";")
+train = pd.read_csv(trainfile, sep=";")
+
+#plot a first overview of the data
 print("\nFirst look at the Data")
 plot.ts_mapplot(dataset=train, grid_rv='boat', grid_cv='pclass', grid_hue='survived', map_v='age')
 plot.ts_surv_prop(dataset=train, xv='age', yv='survived', colv='sex', huev='sex')
 
-print ("\nCleaning up some data")
-
-utils.clean_data(train)
-utils.clean_data(test)
+#to have better results, clean up the data
+print ("\nCleaning up the data")
+utils.clean_data(train,testfile,trainfile)
+utils.clean_data(test,testfile,trainfile)
 
 print ("\nExtracting target and features")
 
 print(train.shape)
 target = train["survived"].values
-features = train[["pclass", "sex", "age", "fare"]].values
-
-decision_tree = tree.DecisionTreeClassifier(random_state = 1)
+feature_names = ["pclass", "age", "sex", "fare", "sibsp", "parch", "embarked", "boat"]
+features = train[feature_names].values
+decision_tree = tree.DecisionTreeClassifier(
+    max_depth = 3,
+    min_samples_split = 2,
+    random_state = 1)
 decision_tree = decision_tree.fit(features, target)
 
 print(decision_tree.feature_importances_)
 print(decision_tree.score(features, target))
+plotfilehandle = open(plotfile, "w")
+tree.export_graphviz(decision_tree, feature_names=feature_names, out_file=plotfilehandle, rounded = True, proportion = False, precision = 2, filled = True)
 
-print ("\nTry on test set")
-
-test_features = test[["pclass", "sex", "age", "fare"]].values
-prediction = decision_tree.predict(test_features)
-utils.write_prediction(prediction, "../submissions/decision_tree.csv")
-# Resultat -> 0.7576923076923077
-
-print ("\nCorrect overfitting")
-
-feature_names = ["pclass", "age", "sex", "fare", "sibsp", "parch", "embarked", "boat"]
-features_two = train[feature_names].values
-decision_tree_two = tree.DecisionTreeClassifier(
-    max_depth = 3,
-    min_samples_split = 2,
-    random_state = 1)
-decision_tree_two = decision_tree_two.fit(features_two, target)
-
-print(decision_tree_two.feature_importances_)
-print(decision_tree_two.score(features_two, target))
-tree.export_graphviz(decision_tree_two, feature_names=feature_names, out_file="decision_tree_two.dot", rounded = True, proportion = False, precision = 2, filled = True)
-
-scores = model_selection.cross_val_score(decision_tree_two, features_two, target, scoring='accuracy', cv=10)
+scores = model_selection.cross_val_score(decision_tree, features, target, scoring='accuracy', cv=10)
 print (scores)
 print (scores.mean())
 
 print ("\nWrite new predicition")
 
-test_features_two = test[["pclass", "age", "sex", "fare", "sibsp", "parch", "embarked", "boat"]].values
-prediction_two = decision_tree_two.predict(test_features_two)
-utils.write_prediction(prediction_two, "../submissions/Rose_and_Jack_post.csv")
-# Resultat ->  
+test_features = test[["pclass", "age", "sex", "fare", "sibsp", "parch", "embarked", "boat"]].values
+prediction = decision_tree.predict(test_features)
+utils.write_prediction(prediction, testfile, submissionfile)
+# Resultat -> 0.973158
--- Titanic/scripts/l_utils.py
+++ Titanic/scripts/l_utils.py
@@ -8,20 +8,11 @@ Created on Wed Oct 24 20:26:12 2018
 import numpy as np
 import pandas as pd
 
-test = pd.read_csv('../data/titanic3_test.csv', sep=';')
-train = pd.read_csv('../data/titanic3_train.csv', sep=';')
+test = pd.DataFrame
 
-print(test.mean())
-print(train.mean())
+def clean_data(data,testfile,trainfile):
 
-
-
-
-def clean_data(data):
-    test = pd.read_csv('../data/titanic3_test.csv', sep=';')
-    train = pd.read_csv('../data/titanic3_train.csv', sep=';')
-
-    combine = pd.concat([train, test])
+    combine = pd.concat([pd.read_csv(testfile, sep=";"), pd.read_csv(trainfile, sep=";")])
 
     data["fare"] = data["fare"].fillna(combine["fare"].dropna().median())
     data["age"] = data["age"].fillna(combine["age"].dropna().median())
@@ -39,23 +30,23 @@ def clean_data(data):
     data.loc[data["embarked"] == "Q", "embarked"] = 2
     
 #neu
-    data["boat"] = data["boat"].fillna(999)
-    data.loc[data["boat"] == "A", "boat"] = 995
-    data.loc[data["boat"] == "B", "boat"] = 996
-    data.loc[data["boat"] == "C", "boat"] = 997
-    data.loc[data["boat"] == "D", "boat"] = 998
-    data.loc[data["boat"] == "C D", "boat"] = 881
-    data.loc[data["boat"] == "5 9", "boat"] = 990
-    data.loc[data["boat"] == "5 7", "boat"] = 991
-    data.loc[data["boat"] == "8 10", "boat"] = 992
-    data.loc[data["boat"] == "13 15 B", "boat"] = 993
-    data.loc[data["boat"] == "13 15", "boat"] = 880
-    data.loc[data["boat"] == "15 16", "boat"] = 994
-    
-    
+    data["boat"] = data["boat"].fillna(0)
+    data.loc[data["boat"] == "A", "boat"] = 17
+    data.loc[data["boat"] == "B", "boat"] = 18
+    data.loc[data["boat"] == "C", "boat"] = 19
+    data.loc[data["boat"] == "D", "boat"] = 20
+    data.loc[data["boat"] == "C D", "boat"] = 21
+    data.loc[data["boat"] == "5 9", "boat"] = 21
+    data.loc[data["boat"] == "5 7", "boat"] = 21
+    data.loc[data["boat"] == "8 10", "boat"] = 21
+    data.loc[data["boat"] == "13 15 B", "boat"] = 21
+    data.loc[data["boat"] == "13 15", "boat"] = 21
+    data.loc[data["boat"] == "15 16", "boat"] = 21
     
 
-def write_prediction(prediction, name):
+def write_prediction(prediction, testfile, predictionfile):
+    print("write prediction")
+    test = pd.read_csv(testfile, sep=";")
     PassengerId = np.array(test["id"]).astype(int)
     solution = pd.DataFrame(prediction, PassengerId, columns = ["survived"])
-    solution.to_csv(name, index_label = ["id"])
+    solution.to_csv(predictionfile, index_label = ["id"], sep=";")
